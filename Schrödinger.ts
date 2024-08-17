4aaadd079fdcef9cfcc492c2e74e4fcf17aeb369
AbdelrahmanKhaled22 <abdelrahmankhaled25@gmail.com>
2024-08-17 10:58:30 +0300

    reduced redundancy

--- Schrödinger.ts
+++ Schrödinger.ts
@@ -1,61 +1,42 @@
-enum Framestate {
+enum FrameState {
   NoCat = 0,
-  CatDetected = 1,
+  CatPresent = 1,
 }
+
 export class CatorNoCat {
-  private requiredframes: number;
+  private requiredFrames: number;
   private stack: number[] = [];
-  private state: Framestate = Framestate.NoCat;
-  constructor(requiredframes: number) {
-    if (requiredframes <= 0) {
+  private state: FrameState = FrameState.NoCat;
+
+  constructor(requiredFrames: number) {
+    if (requiredFrames <= 0) {
       throw new Error("Required frames must be a positive integer");
     }
-    this.requiredframes = requiredframes;
+    this.requiredFrames = requiredFrames;
   }
-  shouldChangeState(frame: Framestate): Framestate {
-    // When there is no cat im waiting for the cat frame which is 1
-    if (this.state === Framestate.NoCat) {
-      if (frame === Framestate.NoCat) {
-        this.stack.pop();
-        // console.log(this.stack);
-        return 0;
+
+  shouldChangeState(frame: FrameState): FrameState {
+    const isStateChangeDetected = this.processFrame(frame);
+    return isStateChangeDetected ? FrameState.CatPresent : FrameState.NoCat;
+  }
+
+  private processFrame(frame: FrameState): boolean {
+    const expectedFrame =
+      this.state === FrameState.NoCat
+        ? FrameState.CatPresent
+        : FrameState.NoCat;
+
+    if (frame === expectedFrame) {
+      this.stack.push(frame);
+      if (this.stack.length === this.requiredFrames) {
+        this.state = expectedFrame;
+        this.stack = [];
+        return true;
       }
-      if (frame === Framestate.CatDetected) {
-        this.stack.push(frame);
-        // console.log(this.stack);
-        if (this.stack.length === this.requiredframes) {
-          this.state = Framestate.CatDetected;
-          this.stack.length = 0;
-          //   console.log(this.stack);
-          return 1;
-        } else {
-          return 0;
-        }
-      }
+    } else {
+      this.stack.pop();
     }
 
-    // When there is a cat im waiting for the no cat frame which is 0
-    if (this.state === Framestate.CatDetected) {
-      if (frame === Framestate.CatDetected) {
-        this.stack.pop();
-        // console.log(this.stack);
-        return 0;
-      }
-      if (frame === Framestate.NoCat) {
-        this.stack.push(frame);
-        // console.log(this.stack);
-
-        if (this.stack.length === this.requiredframes) {
-          this.state = Framestate.NoCat;
-          this.stack.length = 0;
-          //   console.log(this.stack);
-
-          return 1;
-        } else {
-          return 0;
-        }
-      }
-    }
-    throw new Error("Unreachable code reached");
+    return false;
   }
 }
