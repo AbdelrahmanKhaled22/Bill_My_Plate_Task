6244ea2a9275e96c6d46bd77731f586a6fa269c9
AbdelrahmanKhaled22 <abdelrahmankhaled25@gmail.com>
2024-08-17 09:48:06 +0300

    enhanced readability by changing type to enum

--- Schrödinger.ts
+++ Schrödinger.ts
@@ -1,24 +1,27 @@
-type MyBoolean = 0 | 1;
+enum Framestate {
+  NoCat = 0,
+  CatDetected = 1,
+}
 export class CatorNoCat {
   private requiredframes: number;
   private stack: number[] = [];
-  private state: MyBoolean = 0;
+  private state: Framestate = Framestate.NoCat;
   constructor(requiredframes: number) {
     this.requiredframes = requiredframes;
   }
-  shouldChangeState(frame: MyBoolean): MyBoolean {
+  shouldChangeState(frame: Framestate): Framestate {
     // When there is no cat im waiting for the cat frame which is 1
-    if (this.state === 0) {
-      if (frame === 0) {
+    if (this.state === Framestate.NoCat) {
+      if (frame === Framestate.NoCat) {
         this.stack.pop();
         // console.log(this.stack);
         return 0;
       }
-      if (frame === 1) {
+      if (frame === Framestate.CatDetected) {
         this.stack.push(1);
         // console.log(this.stack);
         if (this.stack.length === this.requiredframes) {
-          this.state = 1;
+          this.state = Framestate.CatDetected;
           this.stack.length = 0;
           //   console.log(this.stack);
           return 1;
@@ -29,18 +32,18 @@ export class CatorNoCat {
     }
 
     // When there is a cat im waiting for the no cat frame which is 0
-    if (this.state === 1) {
-      if (frame === 1) {
+    if (this.state === Framestate.CatDetected) {
+      if (frame === Framestate.CatDetected) {
         this.stack.pop();
         // console.log(this.stack);
         return 0;
       }
-      if (frame === 0) {
+      if (frame === Framestate.NoCat) {
         this.stack.push(0);
         // console.log(this.stack);
 
         if (this.stack.length === this.requiredframes) {
-          this.state = 0;
+          this.state = Framestate.NoCat;
           this.stack.length = 0;
           //   console.log(this.stack);
 
